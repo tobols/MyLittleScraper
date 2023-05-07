db8ced9f382bace478d68ba31134051ace39329c
AzureAD\TobiasOlsson <tobias.olsson@isotop.se>
2023-05-07 16:47:15 +0200

    first!

---
+++ MyLittleScraper/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+string choice = null;
+
+do
+{
+    Console.Write("Url: ");
+    choice = Console.ReadLine();
+} while (choice == null);
+
+
+Console.WriteLine(choice);
+
+
